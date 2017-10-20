b73885723a5495ccb7d610925ecf8a7ea6a7d6fe
TaoSun2016 <kevinsun9516@gmail.com>
2017-10-20 20:35:55 +1300

    Add class Customer

---
+++ KeysOnboarding/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KeysOnboarding.Models
+{
+    public class Customer
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string   Address { get; set; }
+        public virtual List<ProductSold> ProductSolds { get; set; }
+    }
+}
