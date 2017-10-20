3003818f1b0e03ceb1b4e8a1edd3aca721b4501d
TaoSun2016 <kevinsun9516@gmail.com>
2017-10-20 20:34:46 +1300

    Modify class Product and ProductSold

--- KeysOnboarding/Models/Product.cs
+++ KeysOnboarding/Models/Product.cs
@@ -8,7 +8,6 @@ namespace KeysOnboarding.Models
     public class Product
     {
         public virtual int Id { get; set; }
-        public virtual int ProductSoldId { get; set; }
         public virtual string Name { get; set; }
         public virtual decimal Price { get; set; }
         public virtual List<ProductSold> ProductSolds { get; set; }
--- KeysOnboarding/Models/ProductSold.cs
+++ KeysOnboarding/Models/ProductSold.cs
@@ -12,6 +12,9 @@ namespace KeysOnboarding.Models
         public virtual int CustomerId { get; set; }
         public virtual int StoreId { get; set; }
         public virtual  DateTime DateSold { get; set; }
+        public virtual  Product Product { get; set; }
+        public virtual Customer Customer { get; set; }
+        public virtual Store Store { get; set; }
 
     }
 }
