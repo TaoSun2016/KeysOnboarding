2d906378eca6c655f334f33c240cf1f8288f8793
TaoSun2016 <kevinsun9516@gmail.com>
2017-10-20 20:28:33 +1300

    Add class ProductSold

--- KeysOnboarding/Models/Product.cs
+++ KeysOnboarding/Models/Product.cs
@@ -8,7 +8,7 @@ namespace KeysOnboarding.Models
     public class Product
     {
         public virtual int Id { get; set; }
-        public virtual int SoldId { get; set; }
+        public virtual int ProductSoldId { get; set; }
         public virtual string Name { get; set; }
         public virtual decimal Price { get; set; }
         public virtual List<ProductSold> ProductSolds { get; set; }
---
+++ KeysOnboarding/Models/ProductSold.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KeysOnboarding.Models
+{
+    public class ProductSold
+    {
+        public virtual int Id { get; set; }
+        public virtual int ProductId { get; set; }
+        public virtual int CustomerId { get; set; }
+        public virtual int StoreId { get; set; }
+        public virtual  DateTime DateSold { get; set; }
+
+    }
+}
